e3cbad2e1cd005112a57b9d268dbf0956e3e38bd
Abreto Fu <m@abreto.net>
2017-04-20 17:55:01 +0800

    io plug-in

---
+++ utilities/io.c
@@ -0,0 +1,15 @@
+/* I/O Plug-in, by Abreto <m@abreto.net>. */
+
+#define ISDIGIT(x) ((x>='0')&&(x<='9'))
+void readn(int *n)
+{
+    int c=0;*n=0;
+    for(;!ISDIGIT(c);c=getchar());
+    for(;ISDIGIT(c);c=getchar())*n=(*n)*10+c-'0';
+}
+void putn(int n)
+{
+    int ns[16]={0,n%10},nd=1;
+    while(n/=10)ns[++nd]=n%10;
+    while(nd)putchar(ns[nd--]+'0');
+}
