e775f50724917b82bcfc5357881d95bc8b4831bf
Abreto Fu <m@abreto.net>
2017-07-04 22:03:57 +0800

    naive crt

--- math/euler.c
+++ math/euler.c
@@ -1,15 +1,15 @@
 /* Euler function phi(x), by Abreto<m@abreto.net>. */
 
-#define MAXN    3000000
+#define MAXX    3000000
 
-int phi[MAXN];
+int phi[MAXX];
 void get_euler(void)
 {
     int i = 0, j = 0;
     phi[1] = 1;
-    for(i = 2;i < MAXN;++i)
+    for(i = 2;i < MAXX;++i)
         if(!phi[i])
-            for(j = i;j < MAXN;j += i)
+            for(j = i;j < MAXX;j += i)
             {
                 if(!phi[j]) phi[j] = j;
                 phi[j] = phi[j]/i * (i-1);
