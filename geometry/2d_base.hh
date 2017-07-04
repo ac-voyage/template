384d6f89356eb9949ea52da23e265926dd2289b3
Abreto Fu <m@abreto.net>
2017-07-04 16:52:02 +0800

    2d convex hull finished.

--- geometry/2d_base.hh
+++ geometry/2d_base.hh
@@ -1,8 +1,16 @@
 /* 2D Geometry Base, by Abreto <m@abreto.net>. */
 
 #include <vector>
+#include <cmath>
 
 using namespace std;
+
+#define EPS (1e-8)
+
+bool fless(double a, double b)
+{
+    return ((a-b) < -EPS);
+}
 
 struct point_t
 {
@@ -22,6 +30,11 @@ typedef point_t vector_t;
 int det(vector_t a, vector_t b)
 {
     return a.x * b.y - a.y * b.x;
+}
+
+double mag(vector_t a)
+{
+    return sqrt((double)a.x*a.x+a.y*(double)a.y);
 }
 
 typedef vector<point_t> vp;
