16443eaedd6d0cbadd317d8b6957d6edc3d2d8fd
Abreto Fu <m@abreto.net>
2017-08-05 17:42:40 +0800

    return what is read

--- utilities/io.c
+++ utilities/io.c
@@ -14,11 +14,12 @@
 #endif
 
 #define ISDIGIT(x) ((x>='0')&&(x<='9'))
-void readn(int *n)
+int readn(int *n)
 {
     int c=0;*n=0;
     for(;!ISDIGIT(c);c=getchar());
     for(;ISDIGIT(c);c=getchar())*n=(*n)*10+c-'0';
+    return (*n);
 }
 void putn(int n)
 {
