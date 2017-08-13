74fca0c5d888af0b2d62ccffdfb442ae392ae314
Abreto Fu <m@abreto.net>
2017-08-13 22:09:24 +0800

    lrjs sa

--- string/sa.cc
+++ string/sa.cc
@@ -133,3 +133,48 @@ namespace HashSuffixArray
 	}
 
 } // end namespace HashSuffixArray
+
+namespace lrj_sa
+{
+	const int MAXN = 1000;
+	char s[MAXN];
+	int sa[MAXN], t[MAXN], t2[MAXN], c[MAXN], n;	/* n seems to be the length of s. */
+	/* every charactor is in [0,m-1] */
+	void build_sa(int m)
+	{
+		int i, *x = t, *y = t2;
+		for(i = 0;i < m;++i) c[i] = 0;
+		for(i = 0;i < n;i++) c[x[i]=s[i]]++;
+		for(i = 1;i < m;++i) c[i] += c[i-1];
+		for(i = n-1;i >= 0;--i) sa[--c[x[i]]] = i;
+		for(int k = 1;k <= n;k <<= 1)
+		{
+			int p = 0;
+			for(i = n-k;i < n;++i) y[p++] = i;
+			for(i = 0;i < n;++i) if(sa[i] >= k) y[p++] = sa[i]-k;
+			for(i = 0;i < m;i++) c[i] = 0;
+			for(i = 0;i < n;i++) c[x[y[i]]]++;
+			for(i = 0;i < m;++i) c[i]+=c[i-1];
+			for(i = n-1;i >= 0;--i) sa[--c[x[y[i]]]] = y[i];
+			swap(x,y);
+			p = 1; x[sa[0]] = 0;
+			for(i = 1;i < n;++i)
+				x[sa[i]] = y[sa[i-1]]==y[sa[i]] && y[sa[[i-1]+k]]==y[sa[i]+k] ? p-1:p++;
+			if(p >= n) break;
+			m = p;
+		}
+	}
+	int rank[MAXN], height[MAXN];
+	void get_height(void)
+	{
+		int i,j,k = 0;
+		for(i = 0;i < n;++i) rank[sa[i]] = i;
+		for(i = 0;i < n;++i)
+		{
+			if(k) k--;
+			j = sa[rank[i]-1];
+			while(s[i+k]==s[j+k]) k++;
+			height[rank[i]] = k;
+		}
+	}
+} // end namespace lrj_sa
