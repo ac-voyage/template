d0653d3278fec0379b8d183269de260ab83d92da
UESTC_Duiming <uestc_duiming@xian.icpc>
2017-10-25 13:29:55 +0800

    packaged matrix struct

---
+++ utilities/mat.cc
@@ -0,0 +1,62 @@
+
+template <class T, int maxn> /* template <class T=int/ll, int maxn, ll mod> */
+struct mat
+{
+    int N;
+    T a[maxn][maxn];    /* 1-based. */
+    mat(void):N(0){}
+    mat(int n, int v = 0)
+    {
+        N = n;
+        for(int i = 1;i <= N;++i)
+            for(int j = 1;j <= N;++j)
+                a[i][j] = 0;
+        for(int i = 1;i <= N;++i)
+            a[i][i] = v;
+    }
+    mat operator-(void) const
+    {
+        mat ret(N);
+        for(int i = 1;i <= N;++i)
+            for(int j = 1;j <= N;++j)
+                ret.a[i][j] = -a[i][j]; /* +mod) % mod */
+        return ret;
+    }
+    mat operator+(const mat &b) const
+    {
+        mat ret(N);
+        for(int i = 1;i <= N;++i)
+            for(int j = 1;j <= N;++j)
+                ret.a[i][j] = a[i][j] + b.a[i][j];  /* % mod */
+        return ret;
+    }
+    mat operator-(const mat &b) const
+    {
+        mat ret(N);
+        for(int i = 1;i <= N;++i)
+            for(int j = 1;j <= N;++j)
+                ret.a[i][j] = a[i][j] - b.a[i][j];  /* +mod) % mod */
+        return ret;
+    }
+    mat operator*(const mat &b) const
+    {
+        mat ret(N);
+        for(int i = 1;i <= N;++i)
+            for(int j = 1;j <= N;++j)
+                for(int k = 1;k <= N;++k)
+                    ret.a[i][j] += a[i][k] * b.a[k][j]; /* % mod */
+        return ret;
+    }
+    mat operator^(long long int p) const
+    {
+        mat ret(N,1);
+        mat base = (*this);
+        while(p)
+        {
+            if(p & 1) ret = ret * base;
+            base = base * base;
+            p >>= 1;
+        }
+        return ret;
+    }
+};
