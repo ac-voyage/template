36476f0f3cce1f9fb552103734eec84a22914dbf
Abreto Fu <m@abreto.net>
2018-10-22 15:48:13 +0800

    add circum center

---
+++ geometry/circle.cc
@@ -0,0 +1,10 @@
+
+Point CircumCenter(Point a,Point b,Point c){  //三角形的外心
+    Point cp;
+    double a1 = b.x-a.x,b1 = b.y-a.y,c1 = (a1*a1 + b1*b1)/2;
+    double a2 = c.x-a.x,b2 = c.y-a.y,c2 = (a2*a2 + b2*b2)/2;
+    double d = a1*b2 - a2*b1;
+    cp.x = a.x + (c1*b2-c2*b1)/d;
+    cp.y = a.y + (a1*c2-a2*c1)/d;
+    return cp;
+}
