6d75435127eb74fd961f9be5271760a94475d39f
Abreto Fu <m@abreto.net>
2018-10-24 22:04:52 +0800

    update st

--- dp/steiner-tree.cc
+++ dp/steiner-tree.cc
@@ -1,88 +1,49 @@
-#include<cstdio>
-#include<queue>
-#include<cstring>
-using namespace std;
-const int limit = 1050;
-const int INF = 1e9;
-inline int read() {
-    char c = getchar(); int x = 0, f = 1;
-    while(c < '0' || c > '9') {if(c == '-') f = -1; c = getchar();}
-    while(c >= '0' && c <= '9') {x = x * 10 + c - '0'; c = getchar();}
-    return x * f;
-}
-#define MP(i,j) make_pair(i,j)
-#define se second
-#define fi first
-#define Pair pair<int,int>
-int N, M, tot = 0;
-int a[12][12], f[12][12][limit];
-int xx[5] = {-1, +1, 0, 0};
-int yy[5] = {0, 0, -1, +1};
-int vis[12][12];
-struct PRE {
-    int x, y, S;
-}Pre[12][12][limit];
-queue<Pair>q;
-void SPFA(int cur) {
-    while(q.size() != 0) {
-        Pair p = q.front();q.pop();
-        vis[p.fi][p.se] = 0;
-        for(int i = 0; i <4; i++) {
-            int wx = p.fi + xx[i], wy = p.se + yy[i];
-            if(wx < 1 || wx > N || wy < 1 || wy > M) continue;
-            if(f[wx][wy][cur] > f[p.fi][p.se][cur] + a[wx][wy]) {
-                f[wx][wy][cur] = f[p.fi][p.se][cur] + a[wx][wy];
-                Pre[wx][wy][cur] = (PRE){p.fi, p.se, cur};
-                if(!vis[wx][wy])
-                    vis[wx][wy] = 1, q.push(MP(wx,wy));
+int f[1<<M][N];
+queue<int> q;
+bool in[N];
+
+void spfa(int S)
+{
+    while (!q.empty())
+    {
+        int now=q.front(); q.pop();
+        in[now]=0;
+        for (int i=st[now];i;i=way[i].nxt)
+        {
+            int y=way[i].y;
+            if (f[S][y]>f[S][now]+val[y])
+            {
+                f[S][y]=f[S][now]+val[y];
+                if (!in[y]) q.push(y),in[y]=1;
             }
         }
     }
 }
-void dfs(int x, int y, int now) {
-    vis[x][y] = 1;
-    PRE tmp = Pre[x][y][now];
-    if(tmp.x == 0 && tmp.y == 0) return;
-    dfs(tmp.x, tmp.y, tmp.S);
-    if(tmp.x == x && tmp.y == y) dfs(tmp.x, tmp.y, now - tmp.S);
-}
-int main() {
-    //freopen("a.in", "r", stdin);
-    N = read(); M = read();
-    memset(f, 0x3f, sizeof(f));
-    for(int i = 1; i <= N; i++)
-        for(int j = 1; j <= M; j++) {
-            a[i][j] = read();
-            if(a[i][j] == 0)
-                f[i][j][1 << tot] = 0, tot++;
-        }
-    int limit = (1 << tot) - 1;
-    for(int sta = 0; sta <= limit; sta++) {
-        for(int i = 1; i<= N; i++)
-            for(int j = 1; j <= M;j++) {
-                for(int s = sta; s; s = (s - 1) & sta) {
-                    if(f[i][j][s] + f[i][j][sta - s] - a[i][j] < f[i][j][sta])
-                        f[i][j][sta] = f[i][j][s] + f[i][j][sta - s] - a[i][j],
-                        Pre[i][j][sta] = (PRE){i,j,s};
-                }
-                if(f[i][j][sta] < INF) q.push(MP(i,j)), vis[i][j] = 1;
-            }
-        SPFA(sta);
+
+void work()
+{
+    int cnt=0;
+    memset(f,0x7f,sizeof(f));
+
+    for (int i=1;i<=n;i++)
+        if (!val[i]) f[1<<cnt][i]=0,cnt++;
+    for (int S=1;S<(1<<cnt);S++)
+    {
+        for (s=(S-1)&S;s;s=(s-1)&S) 
+            for (int i=1;i<=n;i++)
+                f[S][i]=min(f[S][i],f[s][i]+f[S^s][i]-val[i]);
+        for (int i=1;i<=n;i++)
+            if (f[S][i]<INF&&!in[i])
+                q.push(i),in[i]=1;
+        spfa(S);
     }
-    int ansx, ansy, flag = 0;
-    for(int i = 1; i <= N && !flag; i++)
-        for(int j = 1; j <= M; j++)
-            if(!a[i][j]) 
-                {ansx = i, ansy = j; flag = 1; break;}
-    printf("%d\n",f[ansx][ansy][limit]); 
-    memset(vis, 0, sizeof(vis));
-    dfs(ansx, ansy, limit);
-    for(int i = 1; i <= N; i++, puts("")) {
-        for(int j = 1; j <= M; j++) {
-            if(a[i][j] == 0) putchar('x');
-            else if(vis[i][j]) putchar('o');
-            else putchar('_');            
-        } 
-    } 
-    return 0;
+
+    int ans=INF;
+    for (int i=1;i<=n;i++) ans=min(ans,f[(1<<cnt)-1][i]);
+    printf("%d\n",ans);
 }
+// --------------------- 
+// 作者：Coco_T_ 
+// 来源：CSDN 
+// 原文：https://blog.csdn.net/wu_tongtong/article/details/78992913 
+// 版权声明：本文为博主原创文章，转载请附上博文链接！
